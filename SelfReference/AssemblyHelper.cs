4627de2c177c948dbd768816a80c4e0fd6b05574
Aliaksandr Kukrash <multiarc@gmail.com>
2016-05-12 13:27:04 +0300

    Initial commit

---
+++ SelfReference/AssemblyHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace SelfReference
+{
+    internal static class AssemblyHelper
+    {
+        private static volatile DependencyContext _dependencyContext;
+
+        private static readonly ConcurrentDictionary<string, Tuple<Assembly, MetadataReference>> AssemblyCache =
+            new ConcurrentDictionary<string, Tuple<Assembly, MetadataReference>>(StringComparer.OrdinalIgnoreCase);
+
+        private static readonly List<MetadataReference> MetadataReferences;
+
+        private static void WalkReferenceAssemblies(Assembly current)
+        {
+            var currentInfo = new Tuple<Assembly, MetadataReference>(current, CreateMetadataFileReference(current));
+            if (AssemblyCache.TryAdd(current.FullName, currentInfo))
+            {
+                MetadataReferences.Add(currentInfo.Item2);
+                foreach (var assemblyName in current.GetReferencedAssemblies())
+                {
+                    try
+                    {
+                        var dependent = Assembly.Load(assemblyName);
+                        var dependentInfo = new Tuple<Assembly, MetadataReference>(dependent, CreateMetadataFileReference(dependent));
+                        if (AssemblyCache.TryAdd(dependent.FullName, dependentInfo))
+                        {
+                            MetadataReferences.Add(dependentInfo.Item2);
+                            WalkReferenceAssemblies(dependent);
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                    }
+                }
+            }
+        }
+
+        static AssemblyHelper()
+        {
+            var applicationAssembly = Assembly.Load(new AssemblyName(PlatformServices.Default.Application.ApplicationName));
+            _dependencyContext = DependencyContext.Load(applicationAssembly);
+            MetadataReferences = new List<MetadataReference>();
+            WalkReferenceAssemblies(applicationAssembly);
+            GetApplicationReferences();
+        }
+
+        internal static Stream GetResourceStream(Assembly assembly, string name)
+        {
+            return assembly.GetManifestResourceStream(name);
+        }
+
+        private static MetadataReference CreateMetadataFileReference(Assembly asm)
+        {
+            var moduleMetadata = ModuleMetadata.CreateFromFile(asm.Location);
+            var metadata = AssemblyMetadata.Create(moduleMetadata);
+            return metadata.GetReference(filePath: asm.FullName);
+        }
+
+        public static List<MetadataReference> GetApplicationReferences()
+        {
+            if (_dependencyContext != null)
+            {
+                foreach (var name in _dependencyContext.GetDefaultAssemblyNames())
+                {
+                    if (!AssemblyCache.ContainsKey(name.FullName))
+                    {
+                        var asm = Assembly.Load(name);
+                        WalkReferenceAssemblies(asm);
+                    }
+                }
+            }
+            return MetadataReferences;
+        }
+
+    }
+}
---
+++ SelfReference/CompilerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Xunit;
+
+namespace SelfReference
+{
+    public class CompilerTests
+    {
+        [Fact]
+        public void RoslynBadImageFormatRelease()
+        {
+            string code = @"
+    namespace Tests
+    {
+        public static class TestClass
+        {
+            private static void Main(string[] args) { }
+
+            public static object Test(dynamic value)
+            {
+                return value.Count > 0;
+            }
+        }
+    }";
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create(
+                Guid.NewGuid().ToString("N"), new[] {tree},
+                AssemblyHelper.GetApplicationReferences(),
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary).WithOptimizationLevel(OptimizationLevel.Release)
+                    .WithGeneralDiagnosticOption(ReportDiagnostic.Default));
+            var diagnostics = compilation.GetDiagnostics();
+            Assert.False(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error || d.Severity == DiagnosticSeverity.Warning));
+            using (var codeStream = new MemoryStream())
+            {
+                using (var symbolStream = new MemoryStream())
+                {
+                    compilation.Emit(codeStream, symbolStream,
+                        options: new EmitOptions(debugInformationFormat: DebugInformationFormat.PortablePdb));
+                    codeStream.Seek(0, SeekOrigin.Begin);
+                    symbolStream.Seek(0, SeekOrigin.Begin);
+                    Assembly.Load(codeStream.ToArray(), symbolStream.ToArray());
+                }
+            }
+        }
+
+        [Fact]
+        public void RoslynBadImageFormatDebug()
+        {
+            string code = @"
+    namespace Tests
+    {
+        public static class TestClass
+        {
+            private static void Main(string[] args) { }
+
+            public static object Test(dynamic value)
+            {
+                return value.Count > 0;
+            }
+        }
+    }";
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create(
+                Guid.NewGuid().ToString("N"), new[] { tree },
+                AssemblyHelper.GetApplicationReferences(),
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary).WithOptimizationLevel(OptimizationLevel.Debug)
+                    .WithGeneralDiagnosticOption(ReportDiagnostic.Default));
+            var diagnostics = compilation.GetDiagnostics();
+            Assert.False(diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error || d.Severity == DiagnosticSeverity.Warning));
+            using (var codeStream = new MemoryStream())
+            {
+                using (var symbolStream = new MemoryStream())
+                {
+                    compilation.Emit(codeStream, symbolStream,
+                        options: new EmitOptions(debugInformationFormat: DebugInformationFormat.PortablePdb));
+                    codeStream.Seek(0, SeekOrigin.Begin);
+                    symbolStream.Seek(0, SeekOrigin.Begin);
+                    Assembly.Load(codeStream.ToArray(), symbolStream.ToArray());
+                }
+            }
+        }
+    }
+}
